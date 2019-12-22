4b5b51cde4e185b58acef60bda6d523ef9e0d251
Xuelin Gu <xuelingu@umich.edu>
2019-12-22 05:46:50 -0500

    add mixed model crossvalidation

--- linear_mixed.R
+++ linear_mixed.R
@@ -18,6 +18,22 @@ path <- "D:/bios625data"
 
 model_test <- lmer(Reaction ~ Days + (Days | Subject), sleepstudy)
 summary(model_test)
+summary(bigreg_slr)
+PRESS = function(fit){
+  res = residuals(fit)
+  H = hatvalues(fit)
+  sigma = summary(fit)$sigm
+  sres = sapply(1:length(res), function(i) res[[i]]/(sigma*sqrt(1-H[[i]])))
+  PRESS = sum(sres^2)
+  return(PRESS)
+}
+SSY = (fitted(flight_model) - mean(dat$DepDelay))^2
+R_PRESS = 1 - PRESS(flight_model)/SSY
+
+model_test$fitted
+fitted(model_test)
+StudentResid(model_test)
+
 
 #change parameters as needed
 tic("LMM model runtime")
